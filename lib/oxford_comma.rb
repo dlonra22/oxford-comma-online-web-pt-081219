c4929884df3264ac0dd61b586492483eca45b13a
arnie b <arnold.bwaila@gmail.com>
2019-08-22 08:46:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   strng = ""
   cnt = 0
   lngth = array.length
-  lngth -= 1
+  
   array.each do |elemnt|
-    if (cnt == lngth) && (lngth > 1)
+    if (cnt == (lngth-1)) && (lngth > 1)
       strng << "and "+ elemnt
     elsif (lngth > 2)
     strng << elemnt +","
