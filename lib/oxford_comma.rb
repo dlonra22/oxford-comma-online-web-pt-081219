a51d57be2677b0ea9a10d967e0eb82c8ff4a72ae
arnie b <arnold.bwaila@gmail.com>
2019-08-22 08:30:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
   strng = ""
+  cnt =0
   array.each do |elemnt|
-    if cnt == (array.length -1)
+    if (cnt == (array.length -1))
       strng << "and"+ elemnt
     else strng << elemnt +","
     end
+    cnt=+1
   end
   strng
 end
