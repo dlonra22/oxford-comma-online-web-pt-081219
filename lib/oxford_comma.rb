8ec10e0167234f3f3dd4c0dcb28af52a610626df
arnie b <arnold.bwaila@gmail.com>
2019-08-22 09:20:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,12 @@ def oxford_comma(array)
   lngth = array.length
   array.each do |elemnt|
       if (cnt == (lngth)) && (lngth > 1)
-        strng << "and "+ elemnt # executes for 2+ 
+        strng << "and "+ elemnt #executes for 2+ 
       elsif (lngth > 2)
         strng << elemnt +"," #executes for 3+
       else strng << elemnt #executes for 1 element
       end
-      #the below if puts a space for each punctuation + string block unless its the last
+      #the below "if" puts a space for each punctuation + string block unless its the last
       if (cnt < lngth)
         strng << " "
         end
